bd98d954d9c641bc4087e625e8f028b376592d51
zerriet <zerriet@hotmail.sg>
2025-05-03 19:03:51 +0800

    Initial commit

---
+++ src/main/java/com/example/azurebotsample/AzureBotSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.azurebotsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureBotSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureBotSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/azurebotsample/AzureBotSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azurebotsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureBotSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
