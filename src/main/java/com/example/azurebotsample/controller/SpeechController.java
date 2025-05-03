077a597ad7b9a6ea6b6b87606ffc3159e0df7e3a
zerriet <zerriet@hotmail.sg>
2025-05-03 22:52:35 +0800

    speech integration + config

--- src/main/java/com/example/azurebotsample/controller/SpeechController.java
+++ src/main/java/com/example/azurebotsample/controller/SpeechController.java
@@ -4,6 +4,10 @@ import com.example.azurebotsample.model.BaseResponse;
 import com.example.azurebotsample.service.SpeechClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +23,21 @@ public class SpeechController {
      * API call for azure TTS resource
      */
     @PostMapping(value = "get-speech")
-    public BaseResponse getSpeech() {return BaseResponse.builder().build();}
+    public ResponseEntity<byte[]> getSpeech(String speechText) {
+        try {
+            HttpHeaders responseHeaders = new HttpHeaders();
+            // Set the Content-Type to indicate that the body contains WAV audio.
+            byte[] audioBytes = speechClient.generateResponse(speechText);
+            responseHeaders.setContentType(MediaType.valueOf("audio/wav"));
+            // Optionally, set the Content-Length header (good practice)
+            responseHeaders.setContentLength(audioBytes.length);
+            return new ResponseEntity<>(audioBytes, responseHeaders, HttpStatus.OK);
+        }catch (Exception e) {
+            // Handle any exceptions that occur during synthesis
+            System.out.println(e.getMessage());
+            e.printStackTrace(); // Log the error for debugging
+            // Return an appropriate error response
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); // 500 Internal Server Error
+        }
+    }
 }
--- src/main/java/com/example/azurebotsample/service/SpeechClient.java
+++ src/main/java/com/example/azurebotsample/service/SpeechClient.java
@@ -1,14 +1,67 @@
 package com.example.azurebotsample.service;
 
+import com.microsoft.cognitiveservices.speech.*;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.Future;
 
 @Service
 @Slf4j
 @NoArgsConstructor
 public class SpeechClient {
-    private String speechSubscriptionKey = "placedholder";
-    private String resourceRegion = "southeastasia";
-    private String endpointUrl = "https://southeastasia.api.cognitive.microsoft.com/";
+    private final String speechSubscriptionKey = "placedholder";
+    private final String resourceRegion = "southeastasia";
+    private final String endpointUrl = "https://southeastasia.api.cognitive.microsoft.com/";
+    private final String voiceModel = "en-SG-LunaNeural";
+
+    public byte[] generateResponse(String responsePayload){
+        // Creates an instance of a speech synthesizer using speech configuration with
+        // specified
+        // endpoint and subscription key and default speaker as audio output.
+        try(SpeechConfig config = SpeechConfig.fromEndpoint(new java.net.URI(endpointUrl), speechSubscriptionKey)){
+            // Set the voice name, refer to https://aka.ms/speech/voices/neural for full
+            // list.
+            config.setSpeechSynthesisVoiceName(voiceModel);
+            config.setOutputFormat(OutputFormat.Simple);
+            config.setSpeechSynthesisOutputFormat(SpeechSynthesisOutputFormat.Riff16Khz16BitMonoPcm);
+            try (SpeechSynthesizer synth = new SpeechSynthesizer(config)){
+                assert (config != null);
+                assert (synth != null);
+
+                int exitCode = 1;
+
+                log.info("Sending payload to speech service : {}",responsePayload);
+                Future<SpeechSynthesisResult> task = synth.SpeakTextAsync(responsePayload);
+
+                SpeechSynthesisResult result = task.get();
+                assert (result != null);
+
+                if (result.getReason() == ResultReason.SynthesizingAudioCompleted) {
+                    System.out.println("Speech synthesized to speaker for text [" + responsePayload + "]");
+                    exitCode = 0;
+                } else if (result.getReason() == ResultReason.Canceled) {
+                    SpeechSynthesisCancellationDetails cancellation = SpeechSynthesisCancellationDetails
+                            .fromResult(result);
+                    System.out.println("CANCELED: Reason=" + cancellation.getReason());
+
+                    if (cancellation.getReason() == CancellationReason.Error) {
+                        System.out.println("CANCELED: ErrorCode=" + cancellation.getErrorCode());
+                        System.out.println("CANCELED: ErrorDetails=" + cancellation.getErrorDetails());
+                        System.out.println("CANCELED: Did you update the subscription info?");
+                    }
+                }
+                if (exitCode == 0) {
+                    return result.getAudioData();
+                }else {
+                    return null;
+                }
+            }
+
+        } catch (Exception e) {
+            log.info("Unhandled exception : {}", e.getMessage());
+            return null;
+        }
+    }
 }
